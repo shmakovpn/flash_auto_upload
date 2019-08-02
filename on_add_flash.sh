b90533ade51c679f1240e27e3ad9aae85c950dfb
shmakovpn <shmakovpn@yandex.ru>
2019-08-02 11:29:44 +0300

    solved error with checking block device (not a file)

--- on_add_flash.sh
+++ on_add_flash.sh
@@ -13,11 +13,18 @@ fi
 DIR="/media/flash"
 
 logger -p local0.info "'$0' flash device '$1' added 0161392a-b4d1-11e9-ba84-080027858e30"
+
+if [[ ! -b /dev/$1 ]]; then
+    echo "Error: file '/dev/$1' does not exist" >&2
+    logger -p local0.err "'$0' Error: file '/dev/$1' does not exist 0161392a-b4d1-11e9-ba84-080027858e30"
+    exit 1
+fi
 if [[ ! -d ${DIR} ]]; then
     echo "Info: directory '${DIR}' does not exist. Creating it"
     mkdir -p ${DIR}
     if [[ ! -d ${DIR} ]]; then
         echo "Error: could not create '${DIR}'" >&2
+        logger -p local0.err "'$0' Error: could not create '${DIR}' 0161392a-b4d1-11e9-ba84-080027858e30"
         exit 1
     fi
 fi
@@ -26,6 +33,23 @@ if [[ ! -d ${DIR}/$1 ]]; then
     mkdir -p ${DIR}/$1
     if [[ ! -d ${DIR}/$1 ]]; then
         echo "Error: could not create '${DIR}/$1'" >&2
+        logger -p local0.err "'$0' Error: could not create '${DIR}/$1' 0161392a-b4d1-11e9-ba84-080027858e30"
         exit 1
     fi
 fi
+
+mount -l | grep -Pi "^/dev/$1"
+if [[ $? -eq 0 ]]; then
+    echo "Error: '/dev/$1' already mounted" >&2
+    logger -p local0.err "'$0' Error: '/dev/$1' already mounted 0161392a-b4d1-11e9-ba84-080027858e30"
+    exit 1
+fi
+
+mount /dev/$1 ${DIR}/$1
+if [[ $? -ne 0 ]]; then
+    echo "Error: could not mount '/dev/$1'" >&2
+    logger -p local0.err "'$0' Error: could not mount '/dev/$1' 0161392a-b4d1-11e9-ba84-080027858e30"
+    exit 1
+fi
+
+logger -p local0.info "'$0' flash device '$1' mounted to '${DIR}/$1' 0161392a-b4d1-11e9-ba84-080027858e30"
--- on_remove_flash.sh
+++ on_remove_flash.sh
@@ -5,11 +5,32 @@
 logger -p local0.info "'$0' flash device '$1' removed 0161392a-b4d1-11e9-ba84-080027858e30"
 if [[ -z $1 ]]; then
     echo "Error: command line argument \$1 does not present"
+    logger -p local0.err "'$0' first command line argument does not present 0161392a-b4d1-11e9-ba84-080027858e30"
     exit 1
 fi
 if [[ ! $1 =~ ^sd[b-z][1-9]$  ]]; then
-    echo "Error: wrong command line argument '$1'. Must be sd[b-z][1-9]"
+    echo "Error: wrong command line argument '$1'. Must be sd[b-z][1-9]" >&2
+    logger -p local0.err "'$0' Error: wrong command line argument '$1'. Must be sd[b-z][1-9] 0161392a-b4d1-11e9-ba84-080027858e30"
     exit 1
+fi
+
+if [[ -b /dev/$1 ]]; then
+    echo "Error: device '/dev/$1' still exists"
+    logger -p local0.err "'$0' Error: device '/dev/$1' still exists 0161392a-b4d1-11e9-ba84-080027858e30"
+    exit 1
+fi
+
+mount -l | grep -Pi "${DIR}/$1"
+if [[ $? -eq 0 ]]; then
+    echo "Warnign: direcory '${DIR}/$1' is still mounted. Unmounting." >&2
+    logger -p local0.warning "'$0' Error: direcory '${DIR}/$1' is still mounted. Unmounting. 0161392a-b4d1-11e9-ba84-080027858e30"
+    umount ${DIR}/$1
+    mount -l | grep -Pi "${DIR}/$1"
+    if [[ $? -eq 0 ]]; then
+        echo "Error: could not unmount directory '${DIR}/$1'." >&2
+        logger -p local0.err "'$0' Error: could not unmount directory '${DIR}/$1'. 0161392a-b4d1-11e9-ba84-080027858e30"
+        exit 1
+    fi
 fi
 
 DIR="/media/flash"
@@ -18,6 +39,7 @@ if [[ ! -d ${DIR} ]]; then
     mkdir -p ${DIR}
     if [[ ! -d ${DIR} ]]; then
         echo "Error: could not create '${DIR}'" >&2
+        logger -p local0.err "'$0' Error: could not create '${DIR}' 0161392a-b4d1-11e9-ba84-080027858e30"
         exit 1
     fi
 fi
@@ -26,6 +48,9 @@ if [[ -d ${DIR}/$1 ]]; then
     rmdir ${DIR}/$1
     if [[ -d ${DIR}/$1 ]]; then
         echo "Error: could not remove '${DIR}/$1'" >&2
+        logger -p local0.err "'$0' Error: could not remove '${DIR}/$1' 0161392a-b4d1-11e9-ba84-080027858e30"
         exit 1
     fi
 fi
+
+
